66e0e7d855a6c6b4736f9947dfab47f5b8bb48b3
Nathan Walker <walkerrunpdx@gmail.com>
2016-01-31 23:33:33 -0800

    working with delegate

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -1,19 +1,31 @@
 import {Observable} from "data/observable";
 var ezaudio = require("nativescript-ezaudio");
-declare interface EZAudioPlayerDelegate { }
-declare var EZAudioPlayer;
 
-export class AudioPlayerTest extends Observable implements EZAudioPlayerDelegate {
+var EZDelegate = NSObject.extend({
+  playedAudioWithBufferSizeWithNumberOfChannelsInAudioFile(player: any, buffer: number, bufferSize: number, numberOfChannels: number, audioFile: any) {
+    console.log(`buffer: ${buffer}`);
+    console.log(`bufferSize: ${buffer}`);
+    console.log(`numberOfChannels: ${numberOfChannels}`);
+  },
+  updatedPositionInAudioFile(player: any, framePosition: number, audioFile: any) {
+    console.log(`updatedPositionInAudioFile: ${framePosition}`);
+  }
+},
+{
+  protocols: [EZAudioPlayerDelegate]
+});
+
+export class AudioPlayerTest extends Observable {
   public footerNote: string = "<span style='font-family: sans-serif;'>Demo by <a href='https://github.com/NathanWalker'>Nathan Walker</a></span>";
   private player: any;
   private playing: boolean = false;
 
   constructor() {
     super();
-    this.player = EZAudioPlayer.audioPlayerWithDelegate(this);
+    this.player = EZAudioPlayer.audioPlayerWithDelegate(EZDelegate);
     this.set(`playTxt`, `Play Bling`);
   }
-  
+
   public playSound() {
     if (!this.playing) {
       ezaudio.playAudioFile(this.player, `sounds/hotline-bling.mp3`);
@@ -22,7 +34,7 @@ export class AudioPlayerTest extends Observable implements EZAudioPlayerDelegate
     }
     this.togglePlaying();
   }
-  
+
   private togglePlaying() {
     this.playing = !this.playing;
     this.set(`playTxt`, `${this.playing ? 'Stop' : 'Play'} Bling`);
