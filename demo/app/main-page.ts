c74b5f72b18219260176014dada23018d46d810b
Nathan Walker <walkerrunpdx@gmail.com>
2016-02-16 16:52:59 -0800

    working with audioPlot

--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -1,8 +1,11 @@
 var frameModule = require("ui/frame");
+import view = require("ui/core/view");
 import {AudioPlayerDemo} from "./main-view-model";
+
 function pageLoaded(args) {
   var page = args.object;
-  page.bindingContext = new AudioPlayerDemo();
+  var audioPlayer = new AudioPlayerDemo();
+  page.bindingContext = audioPlayer;
   
   var controller = frameModule.topmost().ios.controller;
   var navigationBar = controller.navigationBar;
--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -1,9 +1,12 @@
 import {Observable} from 'data/observable';
+import placeholder = require("ui/placeholder");
+import {Color} from "color";
 import {NSEZAudioPlayer} from 'nativescript-ezaudio';
 
 export class AudioPlayerDemo extends Observable {
   public footerNote: string = "<span style='font-family: sans-serif; color:#999;'>Demo by <a href='https://github.com/NathanWalker'>Nathan Walker</a></span>";
   private player: any;
+  private audioPlot: EZAudioPlot;
   private currentTrackIndex: number = 0;
   private tracks: Array<string> = [
     `sounds/hotline-bling.mp3`,
@@ -25,6 +28,17 @@ export class AudioPlayerDemo extends Observable {
   public swapTrack() {
     this.currentTrackIndex = this.currentTrackIndex === 0 ? 1 : 0;
     this.toggleCurrentTrack(true);
+  }
+  
+  public createAudioPlot(args: placeholder.CreateViewEventData) {
+    console.log('--------- createAudioPlot ---------');
+    this.audioPlot = EZAudioPlot.alloc().initWithFrame(CGRectMake(0, 0, UIApplication.sharedApplication().statusBarFrame.size.width, 300));
+    this.audioPlot.backgroundColor = new Color('#000').ios;
+    this.audioPlot.color = new Color('#FFF803').ios;
+    this.audioPlot.plotType = EZPlotTypeBuffer;
+    this.audioPlot.shouldFill = true;
+    this.audioPlot.shouldMirror = true;
+    args.view = this.audioPlot;
   }
   
   private toggleBtn() {
